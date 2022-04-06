45231bee5e8a5cc2c0f232ecb2a752c36b7f39d1
Nicco Kunzmann <niccokunzmann@rambler.ru>
2022-04-06 13:28:35 +0200

    example script und links

--- example.py
+++ example.py
@@ -13,8 +13,22 @@ If you like to view the repository or recurring_ical_events:
 """
 import icalendar, recurring_ical_events
 from urllib.request import urlopen
-ical_string = urlopen("http://tinyurl.com/y24m3r8f").read()
+
+## Ical file from test cases
+url = "http://tinyurl.com/y24m3r8f"
+date = (2019, 3) # March 2019
+
+## German Holidays
+url = "https://www.calendarlabs.com/ical-calendar/ics/46/Germany_Holidays.ics"
+date = 2022 # year 2022
+
+## FOSSASIA 2022 events
+# from https://api.eventyay.com/v1/events/6b901f56.ics?include_sessions
+url = "https://niccokunzmann.github.io/ical-talk-fossasia-2022/summit.ics"
+date = (2022, 4, 8, 12) # midday at the 8th of April 2022
+
+ical_string = urlopen(url).read()
 calendar = icalendar.Calendar.from_ical(ical_string)
-events = recurring_ical_events.of(calendar).at((2022, 3))
+events = recurring_ical_events.of(calendar).at(date)
 for event in events:
     print("start {} summary {}".format(event["DTSTART"].dt, event.get("SUMMARY")))
