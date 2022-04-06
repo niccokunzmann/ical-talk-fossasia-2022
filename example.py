311a12f5c70e7109b958a95a42e45c8d685c3630
Nicco Kunzmann <niccokunzmann@rambler.ru>
2022-04-06 13:31:23 +0200

    uncomment other calendars

--- example.py
+++ example.py
@@ -7,6 +7,9 @@ To prepare:
 
 You can find the talk at:
     https://niccokunzmann.github.io/ical-talk-fossasia-2022
+  and
+    https://github.com/niccokunzmann/ical-talk-fossasia-2022
+
 If you like to view the repository or recurring_ical_events:
     https://github.com/niccokunzmann/python-recurring-ical-events
 
@@ -19,13 +22,13 @@ url = "http://tinyurl.com/y24m3r8f"
 date = (2019, 3) # March 2019
 
 ## German Holidays
-url = "https://www.calendarlabs.com/ical-calendar/ics/46/Germany_Holidays.ics"
-date = 2022 # year 2022
+#url = "https://www.calendarlabs.com/ical-calendar/ics/46/Germany_Holidays.ics"
+#date = 2022 # year 2022
 
 ## FOSSASIA 2022 events
-# from https://api.eventyay.com/v1/events/6b901f56.ics?include_sessions
-url = "https://niccokunzmann.github.io/ical-talk-fossasia-2022/summit.ics"
-date = (2022, 4, 8, 12) # midday at the 8th of April 2022
+## from https://api.eventyay.com/v1/events/6b901f56.ics?include_sessions
+#url = "https://niccokunzmann.github.io/ical-talk-fossasia-2022/summit.ics"
+#date = (2022, 4, 8, 12) # midday at the 8th of April 2022
 
 ical_string = urlopen(url).read()
 calendar = icalendar.Calendar.from_ical(ical_string)
